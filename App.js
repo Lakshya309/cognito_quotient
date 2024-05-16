bced8b365f681df33114a23c9474759ecb8e4075
unknown <2004lakshya@gmail.com>
2024-05-17 01:11:28 +0530

    final touching 2!

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="test">
+    <Stack.Navigator initialRouteName="home">
     
       <Stack.Screen name="home" component={Home} />
       <Stack.Screen name="options" component={Option} />
